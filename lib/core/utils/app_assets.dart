d79672b96032507f8ac28683da364cafff137dbe
IbrahimHosny2002 <hosnyhema66@gmail.com>
2025-03-01 16:31:33 +0200

    add app assets

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,168 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for images1
+  /// assets/images/1.svg
+  static const String images1 = "assets/images/1.svg";
+
+  /// Assets for images10
+  /// assets/images/10.svg
+  static const String images10 = "assets/images/10.svg";
+
+  /// Assets for images11
+  /// assets/images/11.svg
+  static const String images11 = "assets/images/11.svg";
+
+  /// Assets for images12
+  /// assets/images/12.svg
+  static const String images12 = "assets/images/12.svg";
+
+  /// Assets for images13
+  /// assets/images/13.svg
+  static const String images13 = "assets/images/13.svg";
+
+  /// Assets for images14
+  /// assets/images/14.svg
+  static const String images14 = "assets/images/14.svg";
+
+  /// Assets for images15
+  /// assets/images/15.svg
+  static const String images15 = "assets/images/15.svg";
+
+  /// Assets for images16
+  /// assets/images/16.svg
+  static const String images16 = "assets/images/16.svg";
+
+  /// Assets for images17
+  /// assets/images/17.svg
+  static const String images17 = "assets/images/17.svg";
+
+  /// Assets for images18
+  /// assets/images/18.svg
+  static const String images18 = "assets/images/18.svg";
+
+  /// Assets for images2
+  /// assets/images/2.svg
+  static const String images2 = "assets/images/2.svg";
+
+  /// Assets for images3
+  /// assets/images/3.svg
+  static const String images3 = "assets/images/3.svg";
+
+  /// Assets for images4
+  /// assets/images/4.svg
+  static const String images4 = "assets/images/4.svg";
+
+  /// Assets for images5
+  /// assets/images/5.svg
+  static const String images5 = "assets/images/5.svg";
+
+  /// Assets for images6
+  /// assets/images/6.svg
+  static const String images6 = "assets/images/6.svg";
+
+  /// Assets for images7
+  /// assets/images/7.svg
+  static const String images7 = "assets/images/7.svg";
+
+  /// Assets for images8
+  /// assets/images/8.svg
+  static const String images8 = "assets/images/8.svg";
+
+  /// Assets for images9
+  /// assets/images/9.svg
+  static const String images9 = "assets/images/9.svg";
+
+  /// Assets for imagesBackground1
+  /// assets/images/background1.svg
+  static const String imagesBackground1 = "assets/images/background1.svg";
+
+  /// Assets for imagesBackground2
+  /// assets/images/background2.svg
+  static const String imagesBackground2 = "assets/images/background2.svg";
+
+  /// Assets for imagesBackground3
+  /// assets/images/background3.svg
+  static const String imagesBackground3 = "assets/images/background3.svg";
+
+  /// Assets for imagesBackground4
+  /// assets/images/background4.svg
+  static const String imagesBackground4 = "assets/images/background4.svg";
+
+  /// Assets for imagesBackground5
+  /// assets/images/background5.svg
+  static const String imagesBackground5 = "assets/images/background5.svg";
+
+  /// Assets for imagesBackground6
+  /// assets/images/background6.svg
+  static const String imagesBackground6 = "assets/images/background6.svg";
+
+  /// Assets for imagesIcon1
+  /// assets/images/icon1.svg
+  static const String imagesIcon1 = "assets/images/icon1.svg";
+
+  /// Assets for imagesIcon10
+  /// assets/images/icon10.svg
+  static const String imagesIcon10 = "assets/images/icon10.svg";
+
+  /// Assets for imagesIcon11
+  /// assets/images/icon11.svg
+  static const String imagesIcon11 = "assets/images/icon11.svg";
+
+  /// Assets for imagesIcon12
+  /// assets/images/icon12.svg
+  static const String imagesIcon12 = "assets/images/icon12.svg";
+
+  /// Assets for imagesIcon13
+  /// assets/images/icon13.svg
+  static const String imagesIcon13 = "assets/images/icon13.svg";
+
+  /// Assets for imagesIcon14
+  /// assets/images/icon14.svg
+  static const String imagesIcon14 = "assets/images/icon14.svg";
+
+  /// Assets for imagesIcon15
+  /// assets/images/icon15.svg
+  static const String imagesIcon15 = "assets/images/icon15.svg";
+
+  /// Assets for imagesIcon16
+  /// assets/images/icon16.svg
+  static const String imagesIcon16 = "assets/images/icon16.svg";
+
+  /// Assets for imagesIcon2
+  /// assets/images/icon2.svg
+  static const String imagesIcon2 = "assets/images/icon2.svg";
+
+  /// Assets for imagesIcon3
+  /// assets/images/icon3.svg
+  static const String imagesIcon3 = "assets/images/icon3.svg";
+
+  /// Assets for imagesIcon4
+  /// assets/images/icon4.svg
+  static const String imagesIcon4 = "assets/images/icon4.svg";
+
+  /// Assets for imagesIcon5
+  /// assets/images/icon5.svg
+  static const String imagesIcon5 = "assets/images/icon5.svg";
+
+  /// Assets for imagesIcon6
+  /// assets/images/icon6.svg
+  static const String imagesIcon6 = "assets/images/icon6.svg";
+
+  /// Assets for imagesIcon7
+  /// assets/images/icon7.svg
+  static const String imagesIcon7 = "assets/images/icon7.svg";
+
+  /// Assets for imagesIcon8
+  /// assets/images/icon8.svg
+  static const String imagesIcon8 = "assets/images/icon8.svg";
+
+  /// Assets for imagesIcon9
+  /// assets/images/icon9.svg
+  static const String imagesIcon9 = "assets/images/icon9.svg";
+
+  /// Assets for imagesSplash Screen
+  /// assets/images/Splash Screen.svg
+  static const String imagesSplashScreen = "assets/images/Splash Screen.svg";
+}
