d7e573df46cedf83f15c4c0b17cd5e7c83c72c49
IbrahimHosny2002 <hosnyhema66@gmail.com>
2025-03-01 02:29:48 +0200

    add app fonts

---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static const styleJannaLTBold20 = TextStyle(
+    fontFamily: 'Janna LT',
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+  );
+  static const styleJannaLTBold16 = TextStyle(
+    fontFamily: 'Janna LT',
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+  );
+  static const styleJannaLTBold24 = TextStyle(
+    fontFamily: 'Janna LT',
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+  );
+  static const styleJannaLTBold14 = TextStyle(
+    fontFamily: 'Janna LT',
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+  );
+  static const styleJannaLTBold12 = TextStyle(
+    fontFamily: 'Janna LT',
+    fontSize: 12,
+    fontWeight: FontWeight.w700,
+  );
+  static const styleJannaLTBold36 = TextStyle(
+    fontFamily: 'Janna LT',
+    fontSize: 36,
+    fontWeight: FontWeight.w700,
+  );
+  static const styleJannaLTBold32 = TextStyle(
+    fontFamily: 'Janna LT',
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+  );
+  static const styleJannaLTBold16L0 = TextStyle(
+    fontFamily: 'Janna LT',
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    letterSpacing: 0.0,
+  );
+  static const styleJannaLTBold20L200 = TextStyle(
+    fontFamily: 'Janna LT',
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    letterSpacing: 2.0,
+  );
+  static const styleKamaliRegular80 = TextStyle(
+    fontSize: 80,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Kamali',
+  );
+  static const styleArialRegular16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Arial',
+  );
+  static const stylePoppinsRegular16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Poppins',
+  );
+}
